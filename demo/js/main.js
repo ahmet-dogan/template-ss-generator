c570d9934b67815119c1dc58b1c78abff5767d31
ahmetdogan <ahmet@jotform.com>
2020-09-04 10:43:56 +0300

    Form SS :: add logo animation

--- demo/js/main.js
+++ demo/js/main.js
@@ -2,33 +2,41 @@ var colorList = ["#FFCFA4", "#FFD8CF", "#FDDAF3", "#E0D2FF", "#C3F0EB", "#D7EFFC
 var defaultColors = ['rgba(0, 0, 0, 0)', 'rgb(245, 245, 245)', 'rgb(255, 255, 255)'];
 var speed = 1.1;
 
-var getInitialBottomOfImage = function (image) {
-    var $wrapperEl = $(image).closest('.template-wrapper');
-    return $wrapperEl.height() - image.getBoundingClientRect().height;
+var getTopValueOfBottomOfElement = function ($element) {
+    var $wrapperEl = $element.closest('.template-wrapper');
+    return ($element.height() - $wrapperEl.height()) * -1;
 };
-var onImgLoaded = function () {
-    this.style.bottom = getInitialBottomOfImage(this) + 'px';
+
+var calculateMaxDuration = function ($element) {
+    return $element.height() / speed;
 };
-var calculateMaxDuration = function (image) {
-    return image.getBoundingClientRect().height / speed;
-};
-var calculateDuration = function (image, direction) {
-    var imageHeight = image.getBoundingClientRect().height;
-    var currentBottom = Math.abs(parseFloat(image.style.bottom.slice(0, -2)));
-    var maxDuration = calculateMaxDuration(image);
-    var dx = direction === 'up' ? imageHeight - currentBottom : currentBottom;
-    return Math.floor((dx * maxDuration) / imageHeight);
+
+var calculateDuration = function ($element, direction) {
+    var elementHeight = $element.height();
+    var currentTop = Math.abs(parseFloat($element.css('top').slice(0, -2)));
+    var maxDuration = calculateMaxDuration($element);
+    var dx = direction === 'up' ? currentTop : elementHeight - currentTop;
+    return Math.floor((dx * maxDuration) / elementHeight);
 };
 
 $.each(window.results, function (i, result) {
     var $a = $('<a href="https://www.jotform.com/' + result.formID + '" target="_blank"></a>');
     var $backgroundEl = $('<div class="template-bg"></div>');
     var backgroundEl = $backgroundEl.get(0);
-    var $wrapperEl = $('<div class="template-wrapper"></div>');
-    var $image = $('<img class="template-form" src="../' + result.form + '" alt="" />').load(onImgLoaded);
     $a.append($backgroundEl);
+    
+    var $wrapperEl = $('<div class="template-wrapper"></div>');
     $backgroundEl.append($wrapperEl);
-    $wrapperEl.append($image);
+    
+    var $contentEl = $('<div class="template-content"></div>');
+    $wrapperEl.append($contentEl);
+    
+    var $logo = $('<div class="template-logo">LOGO</div>')
+    $contentEl.append($logo);
+    
+    var $image = $('<img class="template-form" src="../' + result.form + '" alt="" />');
+    $contentEl.append($image);
+    
     $('#container').append($a);
     
     if (result.backgroundType === 'image') {
@@ -42,38 +50,50 @@ $.each(window.results, function (i, result) {
     }
     
     $backgroundEl.mouseenter(function () {
-        var $currentImage = $(this).find('img');
-        var currentImage = $currentImage[0];
-        var firstBottom = getInitialBottomOfImage(currentImage);
-        var duration = calculateDuration(currentImage, 'down');
-        $currentImage.stop()
-            .animate({bottom: '0'}, duration, 'linear', function () {
-                    console.log('DONE!!!');
-                    setTimeout(
-                        function () {
-                            $currentImage.animate({bottom: firstBottom}, duration, 'linear');
-                        },
-                        1000
-                    );
-                }
-            );
+        var $this = $(this);
+        var $scrollEl = $this.find('.template-content');
+        var $currentLogo = $this.find('.template-logo');
+        $currentLogo.slideDown();
+        setTimeout(function() {
+            if($this.attr('stop-animation') == 'true') {
+                $this.removeAttr('stop-animation');
+                return;
+            }
+            var topValue = getTopValueOfBottomOfElement($scrollEl);
+            var duration = calculateDuration($scrollEl, 'down');
+            $scrollEl.stop()
+                .animate({top: topValue + 'px'}, duration, 'linear', function () {
+                        setTimeout(
+                            function () {
+                                $scrollEl.animate({top: '0px'}, duration, 'linear');
+                            },
+                            1000
+                        );
+                    }
+                );
+        }, 500);
     });
     
     $backgroundEl.mouseleave(function () {
-        var $currentImage = $(this).find('img');
-        var currentImage = $currentImage[0];
-        var firstBottom = getInitialBottomOfImage(currentImage);
-        var duration = calculateDuration(currentImage, 'up');
-        $currentImage.stop().animate({bottom: firstBottom}, duration, 'linear');
+        var $this = $(this);
+        $this.attr('stop-animation', true);
+        var $scrollEl = $this.find('.template-content');
+        var $currentLogo = $this.find('.template-logo');
+        var duration = calculateDuration($scrollEl, 'up');
+        $currentLogo.slideUp();
+        $scrollEl.stop().animate({top: '0px'}, duration, 'linear');
+        setTimeout(function() {
+            $this.removeAttr('stop-animation');
+        }, 550);
     });
-    
+
     backgroundEl.addEventListener('wheel', function (event) {
         event.preventDefault();
-        var $currentImage = $(this).find('img');
-        var bottomValue = parseFloat($currentImage.stop().css('bottom').slice(0, -2));
-        var calculatedBottomValue = bottomValue + (5 * event.deltaY);
-        calculatedBottomValue = calculatedBottomValue > 0 ? 0 : calculatedBottomValue;
-        calculatedBottomValue = calculatedBottomValue < getInitialBottomOfImage($currentImage[0]) ? getInitialBottomOfImage($currentImage[0]) : calculatedBottomValue;
-        $currentImage.css({bottom: calculatedBottomValue + 'px'});
+        var $scrollEl = $(this).find('.template-content');
+        var topValue = parseFloat($scrollEl.stop().css('top').slice(0, -2));
+        var calculatedTopValue = topValue + (2 * event.deltaY);
+        calculatedTopValue = calculatedTopValue > 0 ? 0 : calculatedTopValue;
+        calculatedTopValue = calculatedTopValue < getTopValueOfBottomOfElement($scrollEl) ? getTopValueOfBottomOfElement($scrollEl) : calculatedTopValue;
+        $scrollEl.css({top: calculatedTopValue + 'px'});
     });
 });
