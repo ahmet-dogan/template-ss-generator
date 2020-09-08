e9ff38e1c0f80cb388a0e85781e65540f9f8263f
ahmetdogan <ahmet@jotform.com>
2020-09-08 08:45:27 +0300

    Form SS :: Add new form.

--- results.js
+++ results.js
@@ -3,98 +3,105 @@ window.results = [
     "background": "20903075693455-bg.jpg",
     "backgroundType": "image",
     "form": "20903075693455-form.png",
-    "processTime": 4650,
+    "processTime": 4370,
     "formID": "20903075693455"
   },
   {
     "background": "20853367294460-bg.jpg",
     "backgroundType": "image",
     "form": "20853367294460-form.png",
-    "processTime": 4395,
+    "processTime": 4360,
     "formID": "20853367294460"
   },
   {
     "background": "20864245705555-bg.jpg",
     "backgroundType": "image",
     "form": "20864245705555-form.png",
-    "processTime": 5815,
+    "processTime": 4762,
     "formID": "20864245705555"
+  },
+  {
+    "background": "rgb(248, 248, 248)",
+    "backgroundType": "color",
+    "form": "21198462005449-form.png",
+    "processTime": 3438,
+    "formID": "21198462005449"
   },
   {
     "background": "rgb(245, 245, 245)",
     "backgroundType": "color",
     "form": "21121721295444-form.png",
-    "processTime": 4459,
+    "processTime": 3037,
     "formID": "21121721295444"
   },
   {
     "background": "rgb(197, 224, 230)",
     "backgroundType": "color",
     "form": "200772726817056-form.png",
-    "processTime": 4201,
+    "processTime": 3441,
     "formID": "200772726817056"
   },
   {
     "background": "rgba(0, 0, 0, 0)",
     "backgroundType": "color",
     "form": "21413914171344-form.png",
-    "processTime": 3332,
+    "processTime": 3118,
     "formID": "21413914171344"
   },
   {
     "background": "rgb(255, 255, 255)",
     "backgroundType": "color",
     "form": "21092334609349-form.png",
-    "processTime": 3568,
+    "processTime": 3308,
     "formID": "21092334609349"
   },
   {
     "background": "rgba(0, 0, 0, 0)",
     "backgroundType": "color",
     "form": "20575198773162-form.png",
-    "processTime": 2764,
+    "processTime": 2864,
     "formID": "20575198773162"
   },
   {
     "background": "93034998944978-bg.jpg",
     "backgroundType": "image",
     "form": "93034998944978-form.png",
-    "processTime": 5231,
+    "processTime": 4923,
     "formID": "93034998944978"
   },
   {
     "background": "93101184159958-bg.jpg",
     "backgroundType": "image",
     "form": "93101184159958-form.png",
-    "processTime": 4910,
+    "processTime": 4513,
     "formID": "93101184159958"
   },
   {
     "background": "rgb(248, 248, 248)",
     "backgroundType": "color",
     "form": "200627186758362-form.png",
-    "processTime": 2246,
+    "processTime": 2258,
     "formID": "200627186758362"
   },
   {
     "background": "21224672668255-bg.jpg",
     "backgroundType": "image",
     "form": "21224672668255-form.png",
-    "processTime": 5928,
+    "processTime": 5900,
     "formID": "21224672668255"
   },
   {
     "background": "rgb(251, 209, 76)",
     "backgroundType": "color",
     "form": "90381893295468-form.png",
-    "processTime": 3685,
+    "processTime": 3846,
     "formID": "90381893295468"
   },
   {
     "background": "rgb(255, 255, 255)",
     "backgroundType": "color",
     "form": "201173375469055-form.png",
-    "processTime": 3301,
+    "processTime": 3287,
     "formID": "201173375469055"
   }
 ]
